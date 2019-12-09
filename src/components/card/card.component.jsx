75e7088cadc24d815bdf7071c339d6f8e40335d5
nearlys40 <nearlysza@gmail.com>
2019-12-09 10:09:39 +0700

    fix src url to show correct image

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,9 +3,7 @@ import './card.styles.css'
 
 export const Card = props => (
     <div className="card-container">
-        <img alt='monster' 
-            // eslint-disable-next-line
-            src={'https://robohash.org/${props.monster.id}?set=set2'}/>
+        <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2`}/>
         <h2> {props.monster.name} </h2>
         <p> {props.monster.email} </p>
     </div>
