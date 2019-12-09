c77d16e4f4f3968feb3840b123cf8bf7a07708e8
nearlys40 <nearlysza@gmail.com>
2019-12-09 10:41:30 +0700

    Updates

---
+++ precache-manifest.30bc684a8dca9aa696c151e3953d04e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3ead6c04a62c3bc530374044b7d3f5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5ea787a5871458de8521",
+    "url": "/monsters-rolodex/static/css/main.aa299e22.chunk.css"
+  },
+  {
+    "revision": "526166e6ab379f24ce06",
+    "url": "/monsters-rolodex/static/js/2.8087c50c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.8087c50c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ea787a5871458de8521",
+    "url": "/monsters-rolodex/static/js/main.8b2c62bb.chunk.js"
+  },
+  {
+    "revision": "c0d3aed6c1e5f899be31",
+    "url": "/monsters-rolodex/static/js/runtime-main.f33520d1.js"
+  }
+]);
