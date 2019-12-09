c29c11e996b64048b7ac2bbe1b8c028b30157093
nearlys40 <nearlysza@gmail.com>
2019-12-09 10:14:49 +0700

    fix img url to show correct image

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,10 @@ import './card.styles.css'
 
 export const Card = props => (
     <div className="card-container">
-        <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2`}/>
+        <img
+            alt='monster'
+            src={`https://robohash.org/${props.monster.id}?set=set2`}
+        />
         <h2> {props.monster.name} </h2>
         <p> {props.monster.email} </p>
     </div>
